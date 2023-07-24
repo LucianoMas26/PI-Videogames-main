2ed130fd94757f5e44287b4a28fbd7b7766bf6e8
Luciano <lucianoalejandromas@gmail.com>
2023-07-24 18:01:10 -0300

    "changes"

--- api/index.js
+++ api/index.js
@@ -19,11 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js")
 const { conn } = require("./src/db.js")
+require("dotenv").config()
 
 // Syncing all the models at once.
 
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log("Is listening at 3001") // eslint-disable-line no-console
   })
 })
